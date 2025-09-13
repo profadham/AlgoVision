4a6cb3cae86ae96a22734249a6dc87364035d91f
profadham <jalaaladham@gmail.com>
2025-09-13 14:54:09 +0300

    com

--- AlgoVision-App/src/App.jsx
+++ AlgoVision-App/src/App.jsx
@@ -224,7 +224,7 @@ function App() {
       </div>
 
       {/* Right half */}
-      <div style={{ width: "80%" }}>
+      <div style={{ width: "80%", background: "#000000" }}>
         <Board
           setWidthPx={setWidthPx}
           widthPx={widthPx}
